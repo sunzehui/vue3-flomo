563df2833bc40ab35ddb9f7121f1c3376fbb9848
阿辉 <oncwnuEXKikeT96xJEFPh9GH37UM@git.weixin.qq.com>
2022-03-16 19:34:47 +0800

    user-title user-satistical 完成

---
+++ src/components/UserStatistical.vue
@@ -0,0 +1,44 @@
+<script lang="ts" setup>
+import { defineProps, toRefs } from "vue";
+const Props = defineProps<{
+  memo: number;
+  tag: number;
+  day: number;
+}>();
+const { memo, tag, day } = toRefs(Props);
+</script>
+<template>
+  <div class="statistical">
+    <div class="memo-num num-jar">
+      <div class="num">{{ memo }}</div>
+      <span>MEMO</span>
+    </div>
+    <div class="tag-num num-jar">
+      <div class="num">{{ tag }}</div>
+      <span>TAG</span>
+    </div>
+    <div class="day-num num-jar">
+      <div class="num">{{ day }}</div>
+      <span>DAY</span>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.statistical {
+  @apply w-full flex justify-between;
+  .num-jar {
+    .num {
+      font-weight: bold;
+      font-size: 24px;
+      line-height: 1;
+      color: #9d9d9d;
+    }
+    span {
+      font-size: 10px;
+      color: #9d9d9d;
+      line-height: 2;
+    }
+  }
+}
+</style>
--- src/views/Mine.vue
+++ src/views/Mine.vue
@@ -1,103 +1,22 @@
+<script lang="ts" setup>
+import UserTitle from "../components/UserTitle.vue";
+import UserStatistical from "../components/UserStatistical.vue";
+import {
+  Menu,
+  Comment,
+  TrendCharts,
+  ArrowRight,
+  DeleteFilled,
+  Refresh,
+  Search,
+} from "@element-plus/icons";
+</script>
+
 <template>
   <div class="wrapper">
     <div class="left">
-      <div class="user">
-        <span class="username">
-          <span>sunzehui</span>
-
-          <span class="pro active">PRO</span>
-        </span>
-        <div class="flex items-center">
-          <div class="notify mr-2">
-            <svg
-              data-v-356cff01=""
-              width="18"
-              height="18"
-              viewBox="0 0 48 48"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <rect
-                data-v-356cff01=""
-                width="48"
-                height="48"
-                fill="white"
-                fill-opacity="0.01"
-              ></rect>
-              <path
-                data-v-356cff01=""
-                d="M24 4C16.268 4 10 10.268 10 18V38H38V18C38 10.268 31.732 4 24 4Z"
-                fill="none"
-              ></path>
-              <path
-                data-v-356cff01=""
-                d="M10 38V18C10 10.268 16.268 4 24 4C31.732 4 38 10.268 38 18V38M4 38H44"
-                stroke="#808080"
-                stroke-width="3"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              ></path>
-              <path
-                data-v-356cff01=""
-                d="M24 44C26.7614 44 29 41.7614 29 39V38H19V39C19 41.7614 21.2386 44 24 44Z"
-                fill="none"
-                stroke="#808080"
-                stroke-width="3"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              ></path>
-            </svg>
-          </div>
-          <div class="setting">
-            <svg
-              data-v-356cff01=""
-              width="20"
-              height="20"
-              viewBox="0 0 48 48"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <rect
-                data-v-356cff01=""
-                width="48"
-                height="48"
-                fill="white"
-                fill-opacity="0.01"
-              ></rect>
-              <path
-                data-v-356cff01=""
-                d="M34.0003 41L44 24L34.0003 7H14.0002L4 24L14.0002 41H34.0003Z"
-                fill="none"
-                stroke="#808080"
-                stroke-width="3"
-                stroke-linejoin="round"
-              ></path>
-              <path
-                data-v-356cff01=""
-                d="M24 29C26.7614 29 29 26.7614 29 24C29 21.2386 26.7614 19 24 19C21.2386 19 19 21.2386 19 24C19 26.7614 21.2386 29 24 29Z"
-                fill="none"
-                stroke="#808080"
-                stroke-width="3"
-                stroke-linejoin="round"
-              ></path>
-            </svg>
-          </div>
-        </div>
-      </div>
-      <div class="statistical">
-        <div class="memo-num num-jar">
-          <div class="num">5</div>
-          <span>MEMO</span>
-        </div>
-        <div class="tag-num num-jar">
-          <div class="num">3</div>
-          <span>TAG</span>
-        </div>
-        <div class="day-num num-jar">
-          <div class="num">4</div>
-          <span>DAY</span>
-        </div>
-      </div>
+      <UserTitle username="sunzehui" :isPro="true" />
+      <UserStatistical :memo="3" :tag="2" :day="3" />
       <div class="checking">这里展示图表</div>
       <ul class="bar-list">
         <li class="active">
@@ -338,52 +257,7 @@ svg {
   .left {
     @apply h-full;
     width: 240px;
-    .user {
-      @apply flex py-3;
-      .username {
-        flex: 1;
-        cursor: pointer;
-        font-weight: bold;
-        font-size: 18px;
-        color: #5f5f5f;
-        .pro {
-          color: white;
-          background: #9d9d9d;
-          font-size: 12px;
-          padding: 2px 4px;
-          border-radius: 4px;
 
-          display: inline-block;
-          position: relative;
-          bottom: 2px;
-          cursor: pointer;
-          @apply mx-1;
-          &.active {
-            background: #212f4b;
-          }
-        }
-      }
-      .notify,
-      .setting {
-        cursor: pointer;
-      }
-    }
-    .statistical {
-      @apply w-full flex justify-between;
-      .num-jar {
-        .num {
-          font-weight: bold;
-          font-size: 24px;
-          line-height: 1;
-          color: #9d9d9d;
-        }
-        span {
-          font-size: 10px;
-          color: #9d9d9d;
-          line-height: 2;
-        }
-      }
-    }
     .checking {
       height: 230px;
       @apply w-full;
@@ -639,15 +513,3 @@ svg {
   }
 }
 </style>
-
-<script setup>
-import {
-  Menu,
-  Comment,
-  TrendCharts,
-  ArrowRight,
-  DeleteFilled,
-  Refresh,
-  Search,
-} from "@element-plus/icons";
-</script>
