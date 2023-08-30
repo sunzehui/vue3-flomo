269ba9e6be255bcac29dce1d8037ddbb2776c1cf
孙泽辉 <501574022@qq.com>
2023-08-30 08:51:38 +0800

    fix: loading 偏移

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,11 @@
 <script setup lang="ts">
-import { ElConfigProvider, ElLoading } from 'element-plus'
-const loadingInstance = ElLoading.service({
-  background: 'rgba(0, 0, 0, 0.1)',
-})
-const resolve = () => {
-  loadingInstance.close()
-}
+import { ElConfigProvider } from 'element-plus'
 </script>
 
 <template>
   <main class="h-screen">
     <ElConfigProvider>
-      <suspense @resolve="resolve">
+      <suspense>
         <template #default>
           <router-view v-slot="{ Component }">
             <component :is="Component" />
